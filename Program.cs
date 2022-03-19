da03c0f8b8e31c86bd6bed5e0d1c4f67bf6ae386
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-03-18 22:39:09 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double n1, n2, n3, media;
+
+Console.WriteLine("Media Aritmética de 3 numeros\n");
+
+Console.Write("Digite o primeiro numero..:");
+n1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o segundo numero...: ");
+n2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Digite o terceiro numero..: ");
+n3 = Convert.ToDouble(Console.ReadLine()!);
+
+media = (n1 + n2 + n3) / 4; 
+
+Console.WriteLine($"Média: {media:N1}");
